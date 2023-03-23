c7104deae8d0a4c8d24a207e04a38b8d5314e04e
jlee14233 <jlee14233@gmail.com>
2023-03-23 22:03:01 +0900

    풀긴햇는데 하..

--- solution/jlee14233/07. divide and conquer/1074.py
+++ solution/jlee14233/07. divide and conquer/1074.py
@@ -27,7 +27,7 @@ r행 c열을 몇 번째로 방문했는지 출력한다.
 1 ≤ N ≤ 15
 0 ≤ r, c < 2N
 '''
-
+'''
 import sys
 from collections import deque
 sys.stdin = open('solution/jlee14233/07. divide and conquer/입력.txt', 'r')
@@ -35,7 +35,7 @@ sys.stdin = open('solution/jlee14233/07. divide and conquer/입력.txt', 'r')
 n,c,r =map(int,sys.stdin.readline().strip().split())
 
 # print(n,c,r)
-'''
+
 전수조사하는 방식이므로 이 방식을 바꿔야한다,
 즉, 어느 특정 부분이 정해져있기 때문에, 그 부분을 파고 들어서 생각하는것이 좋다.
 c,r의 위치에 따라서 특정 위치는 x로 가정한 후 그 다음 숫자들을 탐색하는 방식?
@@ -76,3 +76,108 @@ r,c =1,1 일 때, n값이 변해도 값은 3을 출력해야한다.
 색종이 접기 혹은 쿼드모시깽으로 탐색하는 방식으로 변화시킨다면, 나머지를 0을 주고 그 rc값에만 1을 준다. 탐색을 할 때 0이면 그냥 거기까지 다 더해버린다, 그 이후 탐색부분에서 1이 나올 때까지 숫자를 그냥 더해준다. 1 탐색 직전까지의 숫자를 출력한다.
 
 '''
+'''
+import sys
+from collections import deque
+#sys.stdin = open('solution/jlee14233/07. divide and conquer/입력.txt', 'r')
+
+n,c,r =map(int,sys.stdin.readline().strip().split())
+leng=2**n
+cnt=0
+mat=[[0]*leng for _ in range(leng)]
+mat[c][r]=1
+# print(mat)
+
+def z_find(x,y,n):
+    global cnt, r, c
+    f_1=mat[x][y]
+    for row in range(x,x+n):
+        for col in range(y,y+n):
+            if mat[x][y]==1:
+                print(cnt,end='')
+                quit()
+            elif f_1!=mat[row][col]:
+                z_find(x,y,n//2)
+                z_find(x,y+n//2,n//2)
+                z_find(x+n//2,y,n//2)
+                z_find(x+n//2,y+n//2,n//2)
+                return 0
+    # print(x,y,n)    
+    cnt+=n**2
+
+    return mat
+z_find(0,0,leng)
+
+여기에서 mat을 사용하지 않고 바로 풀이를 할 수 있는 방법
+
+'''
+
+# def z_find(x,y,n):
+#     global cnt, r, c
+
+#     for row in range(x,x+n):
+#         for col in range(y,y+n):
+#             if row==r and col==c:
+#                 print(cnt,end='')
+#                 quit()
+#             if r>n//2 and c>n//2:
+#                 # print(x,y,n)
+#                 z_find(x,y,n//2)
+#                 z_find(x,y+n//2,n//2)
+#                 z_find(x+n//2,y,n//2)
+#                 z_find(x+n//2,y+n//2,n//2)
+#                 return 0
+#     print(x,y,n)    
+#     cnt+=n**2
+
+#     return cnt
+
+import sys
+from collections import deque
+sys.stdin = open('solution/jlee14233/07. divide and conquer/입력.txt', 'r')
+
+# n,r,c =map(int,sys.stdin.readline().strip().split())
+
+# leng=2**n
+# x,y,cnt=0,0,0
+
+# leng=leng//2
+
+# while x!=r and y!=c and leng!=0:
+#     if r>=leng and c>=leng:
+#         cnt+=3*(leng**2)
+#         y+=leng
+#         x+=leng
+#     elif r>=leng and c<leng:
+#         cnt+=2*(leng**2)
+#         y+=leng        
+#     elif r<leng and c>=leng:
+#         cnt+=leng**2
+#         x+=leng
+
+#     leng=leng//2
+#     print(x,y,leng)
+
+# print(cnt)
+
+N, r, c = map(int, input().split())
+
+ans = 0
+
+while N != 0:
+	N -= 1
+	if r < 2 ** N and c < 2 ** N:
+		ans += ( 2 ** N ) * ( 2 ** N ) * 0
+	elif r < 2 ** N and c >= 2 ** N: 
+		ans += ( 2 ** N ) * ( 2 ** N ) * 1
+		c -= ( 2 ** N )
+	elif r >= 2 ** N and c < 2 ** N: 
+		ans += ( 2 ** N ) * ( 2 ** N ) * 2
+		r -= ( 2 ** N )
+	else:
+		ans += ( 2 ** N ) * ( 2 ** N ) * 3
+		r -= ( 2 ** N )
+		c -= ( 2 ** N )
+    
+
+print(ans)
